1791440f60eb1b96a954b02fc4c527eae023fb94
FelipeOropeza <felipe2006.co@gmail.com>
2025-08-22 21:20:03 -0300

    feat: começo

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,6 +1,11 @@
 <div class="min-h-screen flex flex-col justify-between">
-    <main class="flex-1 flex flex-col items-center justify-center">
-        <h1 class="text-3xl font-bold mb-4">FinanTech</h1>
-        <p class="text-lg text-gray-700 mb-8 text-center max-w-xl">Controle seu bolso de forma simples e inteligente. Organize suas contas, categorias e transações em um só lugar!</p>
-    </main>
+    @if(!auth()->check())
+        <main class="flex-1 flex flex-col items-center justify-center">
+            <h1 class="text-3xl font-bold mb-4">FinanTech</h1>
+            <p class="text-lg text-gray-700 mb-8 text-center max-w-xl">Controle seu bolso de forma simples e inteligente. Organize suas contas, categorias e transações em um só lugar!</p>
+        </main>
+    @endauth
+    @auth
+        <livewire:plano />
+    @endauth
 </div>
